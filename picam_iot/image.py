6d5b4cce233e352f9230760876f156c9087c2082
lindelanimcebo <lindelanimcebo@gmail.com>
2020-10-26 13:54:09 +0000

    face detection functionality

--- picam_iot/image.py
+++ picam_iot/image.py
@@ -1,6 +1,9 @@
 from picamera import PiCamera
 from time import sleep
 from storage import storage
+import numpy as np
+import io
+import cv2
 
 class image:
     """
@@ -15,7 +18,8 @@ class image:
         """
         self.camera = PiCamera()
         self.storage = storage()
-        self.storage.set_local(local)
+        #self.storage.set_local(local)
+        self.local = local
     
     def capture(self, name, storage = "local", res = (960, 600)):
         """ Captures and returns and image object.
@@ -32,7 +36,7 @@ class image:
         self.camera.start_preview()
         sleep(5)
         if storage=="local":
-            self.camera.capture(self.storage.get_local()+name)
+            self.camera.capture(self.local + name + ".jpg")
 
         self.camera.stop_preview()
 
@@ -44,10 +48,48 @@ class image:
         """
         self.camera.resolution = res
     
-    def face_capture(self, color = 'blue'):
+    def face_capture(self, name = "image"):
         """ Captures an image and Draws a box around the frame with a face.
-        :param color: The color of the box. Default is blue.
+        :param name: name of the image to be saved, default is "image".
         """
+        stream = io.BytesIO()
+        self.camera.resolution = (320, 240)
+        self.camera.capture(stream, format = 'jpeg')
+        buffer = np.fromstring(stream.getvalue(), dtype = np.uint8)
+
+        img = cv2.imdecode(buffer, 1)
+        cascade = cv2.CascadeClassifier('./res/faces.xml')
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = cascade.detectMultiScale(gray_img, 1.1, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 255), 2)
+        
+        cv2.imwrite( self.local + name + ".jpg", img )
+        self.set_resolution()
+    
+    def face_detect(self, image, show = False):
+        """ Takes an image and detects faces from the image, and draw boxes around where there are faces
+        :param image: location of the image.
+        :param show: whether or not the image should be diplayed to the screen, default is false.
+        
+        """
+        cascade = cv2.CascadeClassifier("./res/faces.xml")
+        img = cv2.imread(image)
+        img = cv2.resize(img, (320, 240))
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = cascade.detectMultiScale(gray_img, 1.1, 4)
+
+        print("found " + str(len(faces)) + " faces")
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        
+        cv2.imwrite(image, img)
+
+        if show:
+            cv2.imshow(image, img)
     
 if __name__ == "__main__":
     pass
